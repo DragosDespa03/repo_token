476abcc7fb7a26cccf1aa600f3d71281b4de30f5
helagami <aitor.luque@educa.madrid.org>
2025-02-11 13:12:47 +0100

    VAitor

--- src/proyecto_token/PROYECTO_TOKEN.java
+++ src/proyecto_token/PROYECTO_TOKEN.java
@@ -16,6 +16,7 @@ public class PROYECTO_TOKEN {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Probando");
     }
     
 }
